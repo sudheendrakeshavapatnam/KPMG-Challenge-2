eb0071f052deea88be7cd81fb05b081f913dd8ea
Sudheendra Keshavapatnam <Sudheendra.keshavapatnam@gmail.com>
2022-09-07 10:27:41 +0530

    Adding Challenge-2 initial commit

---
+++ Challenge3.ps1
@@ -0,0 +1,9 @@
+﻿Login-AzureRmAccount
+
+Select-AzureRmSubscription -Subscription DEVSUB
+
+#Command to get the details of VM and OS details
+(Get-AzureRmVM -ResourceGroupName DEVRG -Name DEVVM  | Select-Object Name,OSProfile) | ConvertTo-Json
+
+#command to get the details related to VM Disk and VM status
+(Get-AzureRmVM -ResourceGroupName DEVRG -Name DEVVM  | Select-Object Name,OSProfile,Disks,Statuses) | ConvertTo-Json
---
+++ README.md
@@ -0,0 +1,5 @@
+# Challange-2
+
+We need to write code that will query the meta data of an instance within AWS/Azure and provide a json formatted output. The choice of language and implementation is up to you.
+
+![image](https://user-images.githubusercontent.com/90386933/132712440-2ec60d72-d361-4ada-b026-280439e7cb5f.png)
